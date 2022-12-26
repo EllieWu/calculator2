58b93a48fabc3d64c8835e4cb7b9ba8a8c98470c
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2022-12-26 17:03:15 +0800

    AAAA

--- app/src/main/java/com/elliewu/calculator2/MainActivity.kt
+++ app/src/main/java/com/elliewu/calculator2/MainActivity.kt
@@ -5,6 +5,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,64 +31,124 @@ class MainActivity : AppCompatActivity() {
             text.setText("0");
         }
         var btn_0 = findViewById<Button>(R.id.button_0)
-        btn_0.setOnClickListener {
-            pikachu(btn_0.getText().toString())
-        }
+        Pikachutest(btn_0);
         var btn_1 = findViewById<Button>(R.id.button_1);
-        btn_1.setOnClickListener {
-            pikachu(btn_1.getText().toString())
-        }
+        Pikachutest(btn_1);
         var btn_2 = findViewById<Button>(R.id.button_2);
-        btn_2.setOnClickListener {
-            pikachu(btn_2.getText().toString())
-        }
+        Pikachutest(btn_2);
         var btn_3 = findViewById<Button>(R.id.button_3);
-        btn_3.setOnClickListener {
-            pikachu(btn_3.getText().toString())
-        }
+        Pikachutest(btn_3);
         var btn_4 = findViewById<Button>(R.id.button_4);
-        btn_4.setOnClickListener {
-            pikachu(btn_4.getText().toString())
-        }
+        Pikachutest(btn_4);
         var btn_5 = findViewById<Button>(R.id.button_5);
-        btn_5.setOnClickListener {
-            pikachu(btn_5.getText().toString())
-        }
+        Pikachutest(btn_5);
         var btn_6 = findViewById<Button>(R.id.button_6);
-        btn_6.setOnClickListener {
-            pikachu(btn_6.getText().toString())
-        }
+        Pikachutest(btn_6);
         var btn_7 = findViewById<Button>(R.id.button_7);
-        btn_7.setOnClickListener {
-            pikachu(btn_7.getText().toString())
-        }
+        Pikachutest(btn_7);
         var btn_8 = findViewById<Button>(R.id.button_8);
-        btn_8.setOnClickListener {
-            pikachu(btn_8.getText().toString())
-        }
+        Pikachutest(btn_8);
         var btn_9 = findViewById<Button>(R.id.button_9);
-        btn_9.setOnClickListener {
-            pikachu(btn_9.getText().toString())
-        }
+        Pikachutest(btn_9);
+
         var btn_dot = findViewById<Button>(R.id.button_dot)
-        btn_dot.setOnClickListener {
-            pikachu(btn_dot.getText().toString())
-        }
+        Pikachutest(btn_dot);
 
-
-    }
-    fun pikachu(pipi:String){
-        var text = findViewById<TextView>(R.id.tvInput);
-        var a = text.getText().toString();
-        if(a == "0"){
-            text.setText(pipi);
-        }
-        else{
-            a = a + pipi;
-            text.setText(a);
+//        var btn_plus = findViewById<CheckBox>(R.id.button_plus)
+//        Caculate(btn_plus);
+        var btn_minus = findViewById<CheckBox>(R.id.button_minus)
+        Caculate(btn_minus);
+        var btn_equal = findViewById<Button>(R.id.button_equal)
+        btn_equal.setOnClickListener{
+            var btn_plus = findViewById<CheckBox>(R.id.button_plus)
+            Log.d("MainActivity","${btn_plus.isChecked()}")
+            Toast.makeText(this, btn_plus.isChecked().toString(), Toast.LENGTH_LONG).show()
         }
     }
-
+    fun Pikachutest(aaa:Button){
+        aaa.setOnClickListener {
+            var text = findViewById<TextView>(R.id.tvInput);
+            var buttontext = aaa.getText().toString();
+            var a = text.getText().toString();
+            if(a == "0"){
+                text.setText(buttontext);
+            }
+            else{
+                a += buttontext;
+                text.setText(a);
+            }
+        }
+    }
+    fun Caculate(bbb:CheckBox)
+    {
+        //Toast.makeText(this, bbb.getId().toString(), Toast.LENGTH_LONG).show()
+        bbb.setOnClickListener{
+            var bb = bbb.getId().toString();
+            Toast.makeText(this, bb, Toast.LENGTH_LONG).show()
+//            var a = bbb.isSelected.toString();
+//            Toast.makeText(this, a, Toast.LENGTH_LONG).show()
+            var buttontext = bbb.getText().toString();
+            var btn_plus = findViewById<CheckBox>(R.id.button_plus);
+            var btn_minus = findViewById<CheckBox>(R.id.button_minus);
+            var btn_multiplied = findViewById<CheckBox>(R.id.button_multiplied);
+            //var btn_division = findViewById<Button>(R.id.button_division);
+            var btn_percent = findViewById<Button>(R.id.button_percent);
+            if(buttontext == "+")
+            {
+                //var a = bbb.onCheckIsTextEditor().toString();
+                //var a = bbb.isTextSelectable.toString();
+                //var a = bbb.stateListAnimator.toString();
+                //var a = bbb.measuredState.toString();
+                //var a = bbb.isDuplicateParentStateEnabled.toString();
+                //var a = bbb.hasTransientState().toString();
+                var a = bbb.isSelected.toString();
+                btn_minus.setSelected(false);
+                btn_multiplied.setSelected(false);
+                //btn_division.setPressed(false);
+                btn_percent.setSelected(false);
+                Toast.makeText(this, a, Toast.LENGTH_LONG).show()
+            }
+            else if(buttontext == "-")
+            {
+                btn_plus.setPressed(false);
+                btn_multiplied.setPressed(false);
+                //btn_division.setPressed(false);
+                btn_percent.setPressed(false);
+                var a = bbb.drawableState.toString();
+                Toast.makeText(this, a, Toast.LENGTH_LONG).show()
+            }
+            else if(buttontext == "×")
+            {
+                btn_minus.setPressed(false);
+                btn_plus.setPressed(false);
+                //btn_division.setPressed(false);
+                btn_percent.setPressed(false);
+                var a = bbb.drawableState.toString();
+                Toast.makeText(this, a, Toast.LENGTH_LONG).show()
+            }
+            else if(buttontext == "÷")
+            {
+                btn_minus.setPressed(false);
+                btn_multiplied.setPressed(false);
+                btn_plus.setPressed(false);
+                btn_percent.setPressed(false);
+                var a = bbb.drawableState.toString();
+                Toast.makeText(this, a, Toast.LENGTH_LONG).show()
+            }
+            else if(buttontext == "%")
+            {
+                btn_minus.setPressed(false);
+                btn_multiplied.setPressed(false);
+                //btn_division.setPressed(false);
+                btn_plus.setPressed(false);
+                var a = bbb.drawableState.toString();
+                Toast.makeText(this, a, Toast.LENGTH_LONG).show()
+            }
+            else if(buttontext == "←")
+            {
+            }
+        }
+    }
     fun findButtonClick (Button:String){
         var text = findViewById<TextView>(R.id.tvInput);
         var a = text.getText().toString();
